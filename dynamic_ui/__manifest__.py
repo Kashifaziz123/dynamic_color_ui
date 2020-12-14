826d7c4098f161eb924aceeab2f3176cde30c5a0
kashif <kashifaziz123@hotmail.com>
2020-12-14 19:35:27 +0500

    Signed-off-by: kashif <kashifaziz123@hotmail.com>

--- dynamic_ui/__manifest__.py
+++ dynamic_ui/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Dynamic Backend Color UI',
     'license': 'AGPL-3',
-    'version': '1.0.1',
+    'version': '13.0.1',
     "category": "Themes/Colors",
     'description': "Dynamic Backend Theme",
     'author': 'AlHadiTech',
