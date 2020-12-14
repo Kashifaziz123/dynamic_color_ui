843536d35428af49c1b93c99518ac2af1eaf61de
kashif <kashifaziz123@hotmail.com>
2020-12-14 19:10:13 +0500

    added first version

---
+++ dynamic_ui/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020-now Al Hadi Tech - Pakistan
+# License AGPL-3.0
+{
+    'name': 'Dynamic Backend Color UI',
+    'license': 'AGPL-3',
+    'version': '1.0.1',
+    "category": "Themes/Colors",
+    'description': "Dynamic Backend Theme",
+    'author': 'AlHadiTech',
+    'price':25,'currency': 'EUR',
+    'website': 'https://alhaditech',
+    'depends': ['base_setup','web'],
+    'images': ['static/description/icon.png'],
+    'data': [
+        "views/assets.xml",
+        "views/template_colors.xml"
+    ],
+    'installable': True,
+    'auto_install': False
+}
